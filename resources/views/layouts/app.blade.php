d88eba4c4f127ee7b1c571487168fb682ef267c8
dikrifzn <dikrifauz@gmail.com>
2025-11-17 21:47:24 +0700

    feat: create base layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ $title ?? 'Cakrawala Event Organizer' }}</title>
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans text-gray-800">
+
+    {{-- Navbar --}}
+    @include('components.navbar')
+
+    {{-- Content --}}
+    <main class="mt-30">
+        @yield('content')
+    </main>
+
+    {{-- Footer --}}
+    @include('components.footer')
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
-
-Route::prefix('shop')->group(function () {
-    Route::get('/', [ShopController::class, 'index']);
-    Route::get('/{slug}', [ShopController::class, 'show']);
+Route::get('/', function () {
+    return view('pages.home');
 });
 
-Route::get('/gallery', [GalleryController::class, 'index']);
+// Route::prefix('shop')->group(function () {
+//     Route::get('/', [ShopController::class, 'index']);
+//     Route::get('/{slug}', [ShopController::class, 'show']);
+// });
 
-Route::prefix('articles')->group(function () {
-    Route::get('/', [ArticleController::class, 'index']);
-    Route::get('/{slug}', [ArticleController::class, 'show']);
-});
+// Route::get('/gallery', [GalleryController::class, 'index']);
 
-Route::view('/about', 'pages.about');
-Route::view('/order/success', 'pages.order.success');
+// Route::prefix('articles')->group(function () {
+//     Route::get('/', [ArticleController::class, 'index']);
+//     Route::get('/{slug}', [ArticleController::class, 'show']);
+// });
+
+// Route::view('/about', 'pages.about');
+// Route::view('/order/success', 'pages.order.success');
